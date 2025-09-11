2aa5b5f4b788fd78287dd129ed92c1f1f61cee4e
Jeffrey <jeffreyvantillo@gmail.com>
2025-09-11 13:03:26 +0200

    Created component for TwitchPlayer and added TwitchPlayer to the Event Detail Page

--- app/event/[id]/page.tsx
+++ app/event/[id]/page.tsx
@@ -1,7 +1,23 @@
+import TwitchPlayer from "@/app/ui/SinglePage/TwitchPlayer";
+
+
+
+// const stream = await fetchStreamByID();
+const stream = "lck";
+
 export default function EventDetailPage() {
   return (
     <>
-        <p>SingleEventPage</p>
+      <TwitchPlayer 
+        channel={stream}
+        autoplay={true} 
+        muted={false} 
+        className="w-full h-70 md:h-screen lg:h-[500px] xl:h-[700px] shadow-lg"
+      />
+
+      <div className="p-4 md:p-8 lg:p-12 xl:p-16">
+        <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-4">Event Title</h1>
+      </div>
     </>
   )
 };
